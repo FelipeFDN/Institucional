f50cfd5ef962f8b8a94725b07ce8560d25b5dbfa
Felipe Daniel Nerling <felipednerling@gmail.com>
2026-03-09 15:08:12 -0300

    Criada a tabela de classes de produtos

---
+++ backend/src/database/migrations/20260309180242-create-product-classes-table.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+    await queryInterface.createTable('product_classes', { 
+        id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          primaryKey: true,
+          defaultValue: Sequelize.UUIDV4
+        },
+        tittle: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        image_url: {
+          type: Sequelize.STRING,
+          allowNull: true
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false
+        }
+      });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('product_classes');
+  }
+};
